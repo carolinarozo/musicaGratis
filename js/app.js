c663c38af82843f07066dd6e3c892e4ee0200fe0
carolinarozo <67168080+carolinarozo@users.noreply.github.com>
2021-04-09 10:59:24 -0500

    últimos detalles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-
+//Funcion para realizar una tabla con los datos de JSON
 function tablaJson() {
-   
+    // Extraer los datos del archivo datos.json
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", 'datos.json', true);
     xhttp.send();
@@ -8,8 +8,10 @@ function tablaJson() {
         if (this.readyState == 4 && this.status == 200) {
             let datos = JSON.parse(this.responseText);
             let datosCanciones = datos.canciones;
+            //ordenar las canciones de mayor a menor en reproducciones
             datosCanciones.sort((a, b) => b.reproducciones - a.reproducciones);
-
+            
+            //Construir la tabla con los datos de JSON solo con los tres primeros elementos de datosCanciones
             let contenido = document.querySelector("#contenido");
             contenido.innerHTML = '';
 
@@ -34,12 +36,17 @@ function tablaJson() {
     
     
 }
+
+//Funcion para limpiar los comentarios que salen en la validacion de formulario
+
 function limpiarErrores(){
   var errores = document.getElementsByClassName("error");
   for(var i = 0; i < errores.length; i++){
     errores[i].innerHTML = "";
   }
 }
+
+//funcion para validar los formularios de la página 
 function validar(formulario) {
 
     limpiarErrores();
@@ -100,7 +107,11 @@ function validar(formulario) {
 
     return true;
 }
+
+
+//Funcion para buscar una cancion con una herramienta de búsqueda
 function buscar() {
+    //Extraer los datos de un archivo JSON
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", '../datos.json', true);
     xhttp.send();
@@ -108,44 +119,44 @@ function buscar() {
         if (this.readyState == 4 && this.status == 200) {
             let datos = JSON.parse(this.responseText);
             let datosCanciones = datos.canciones;
-            
+        
 
             let contenido = document.querySelector("#contenido");
             contenido.innerHTML = '';
 
-            
+        //valor del espacio de buscar
 
             let busqueda = document.getElementById("busqueda").value;
             
            
             
-            
+    // El valor del espacio de buscar se convierte en una expresión regular
             var regexp = new RegExp(busqueda, "i");
 
             
 
             
             
-            
+    //para construir el contenido de la lista de canciones en la pagina de canciones según la búsqueda     
             for (let item of datosCanciones) {         
                 
-                console.log(regexp.test(item.nombre));
+                
 
                 
-                if (regexp.test(item.nombre)) {
+                if (regexp.test(item.nombre)) {// se comprueba que el item tenga las letras del valor de la herramienta de busqueda
                     contenido.innerHTML += `
 
-                    <div class="card col-12 col-md-6 col-lg-4 px-0  bg-secondary">
-                        <div class="m-4 ">
+                    <div class="card col-12 col-md-6 col-lg-4 p-1 bg-secondary border-0">
+                        <div class=" p-5 mt-1 mx-1 border border-bottom-0 shadow">
 
                             <img class="card-img-top img-fluid" src="../images/icon_${item.icono}.svg" alt="Card image cap">
                         </div>
-                        <div class="card-body bg-white px-0">
+                        <div class="card-body bg-white px-5 mx-1 shadow">
                             <h5 class="card-title text-center">${item.nombre}</h5>
 
                             <div class="d-flex justify-content-center">
 
-                            <audio controls class="w-75" src="../canciones/cinco.mp3"></audio>
+                            <audio controls class="w-100" src="../canciones/${item.ruta}"></audio>
 
                             </div>
 
@@ -176,5 +187,4 @@ function buscar() {
 
 
 }
-
 
